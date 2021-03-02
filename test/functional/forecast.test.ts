adcce2ae9395a78ac45a45a3f2b3c13515de5cb3
Leonardo Souza <llsouza.br@gmail.com>
2021-03-02 18:31:01 -0300

    Cleaning code

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -45,4 +45,22 @@ describe('Beach forecast functional tests', () => {
     // Make sure we use toEqual to check value not the object and array itself
     expect(body).toEqual(apiForecastResponse1BeachFixture);
   });
+
+  it('should return 500 if something goes wrong during the processing', async () => {
+    nock('https://api.stormglass.io:443', {
+      encodedQueryParams: true,
+      reqheaders: {
+        Authorization: (): boolean => true,
+      },
+    })
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/v1/weather/point')
+      .query({ lat: '-33.792726', lng: '151.289824' })
+      .replyWithError('Something went wrong');
+
+    const { status } = await global.testRequest.get(`/forecast`);
+
+    expect(status).toBe(500);
+  });
+  
 });
